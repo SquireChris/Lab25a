ffdcea0a5dae81013e50e28f07edd84de803be53
Orion (Theunkn0wn1) <theunkn0wn1@users.noreply.github.com>
2018-04-22 19:38:54 -0700

    signature documentation (part I)

--- functions.h
+++ functions.h
@@ -18,35 +18,45 @@ using namespace std;
  *      Outputs the average of the elements of the array
  *          ==> returns index: the position of the item's first occurrence
  ***************************************************************************/
-double AverageIntArray(const int ARR[], int arrLen);
+double AverageIntArray(const int ARR[], // IN - integer array
+                       int arrLen);     // IN - Array length
+
 
 /***************************************************************************
  * FindFirstInstance
  *      Outputs the index the first time input integer occurs within the array
  *          ==> returns index: the position of the item's first occurrence
  ***************************************************************************/
-int FindFirstInstance(const int ARR[], int arrLen, int searchInt);
+int FindFirstInstance(const int ARR[], // IN - integer array
+                      int arrLen,      // IN - length of array
+                      int searchInt);  // IN - int to search for
 
 /***************************************************************************
  * FindOccurrencesInt
  *      Outputs the number of times an int occurs within the input array
  *          ==> returns occurrences: the number of times an int occurs
  ***************************************************************************/
-int FindOccurrencesInt(const int ARR[], int arrLen, int searchInt);
+int FindOccurrencesInt(const int ARR[], // IN - integer array
+                       int arrLen,      // IN - length of array
+                       int searchInt);  // IN - int to search for
 
 /***************************************************************************
  * FindLargestInt
  *      Outputs the index of the largest element within the input array
  *          ==> returns minIndex: largest element's index
  ***************************************************************************/
-int FindLargestInt(const int ARR[], int arrLen);
+int FindLargestInt(const int ARR[], // IN - integer array
+                   int arrLen);     // IN - Array length
+
 
 /***************************************************************************
  * FindString
  *      Outputs the index of the searched string within an array
  *          ==> returns Index: the index of the searched name
  ***************************************************************************/
-int FindString(const string ARR[], int arrLen, const string &SEARCH_STR);
+int FindString(const int ARR[],             // IN - integer array
+               int arrLen,                  // IN - Array length
+               const string &SEARCH_STR);   // IN - string to search for
 
 /***************************************************************************
  * FindSmallestInt
@@ -54,7 +64,8 @@ int FindString(const string ARR[], int arrLen, const string &SEARCH_STR);
  *      Outputs the index of the smallest element within the input array
  *          ==> returns minIndex: smallest element's index
  ***************************************************************************/
-int FindSmallestInt(const int ARR[], int arrLen);
+int FindSmallestInt(const int ARR[], // IN - integer array
+                    int arrLen);     // IN - Array length
 
 /***************************************************************************
  * SumIntArray
@@ -62,7 +73,8 @@ int FindSmallestInt(const int ARR[], int arrLen);
  *
  *      ==> returns sum : sum of array
  ***************************************************************************/
-int SumIntArray(const int ARR[], int arrLen);
+int SumIntArray(const int ARR[], // IN - integer array
+                int arrLen);     // IN - Array length
 
 
 #endif /* functions */
